da35a8544506201d1411de6cdc864e343be96334
whatchuThinkin <spearhead0427@gmail.com>
2022-12-02 16:42:51 +0900

    Add files via upload

---
+++ examples/examplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  examplesApp.swift
+//  examples
+//
+//  Created by 1100690 on 2022/12/02.
+//
+
+import SwiftUI
+
+@main
+struct examplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
